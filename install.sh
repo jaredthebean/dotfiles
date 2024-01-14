3fe037b8344bd0783d68570fa2cc1d763460c19e
Jared Bean <jibean19@gmail.com>
2024-01-14 15:43:55 -0500

    Force rc overwrite

--- install.sh
+++ install.sh
@@ -8,17 +8,17 @@ readonly SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 mkdir -p "${CONFIG_DIR}"
 
 # Git
-ln -s "${SCRIPT_DIR}/git" "${CONFIG_DIR}/git"
+ln -fs "${SCRIPT_DIR}/git" "${CONFIG_DIR}/git"
 
 # Vim
-ln -s "${SCRIPT_DIR}/vim/rc" "${HOME}/.vimrc"
+ln -fs "${SCRIPT_DIR}/vim/rc" "${HOME}/.vimrc"
 mkdir -p "${HOME}/.vim"
-ln -s "${SCRIPT_DIR}/vim/autoload" "${HOME}/.vim/autoload"
+ln -fs "${SCRIPT_DIR}/vim/autoload" "${HOME}/.vim/autoload"
 mkdir -p "${HOME}/.vim/plugged"
 
 # Kitty
-ln -s "${SCRIPT_DIR}/kitty" "${CONFIG_DIR}/kitty"
+ln -fs "${SCRIPT_DIR}/kitty" "${CONFIG_DIR}/kitty"
 
 # ZSH
-ln -s "${CONFIG_DIR}/zsh/rc" "${HOME}/.zshrc"
-ln -s "${CONFIG_DIR}/zsh/aliases" "${HOME}/.zsh_aliases"
+ln -fs "${CONFIG_DIR}/zsh/rc" "${HOME}/.zshrc"
+ln -fs "${CONFIG_DIR}/zsh/aliases" "${HOME}/.zsh_aliases"
