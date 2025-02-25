ce6dece7129599d1e73b498a753cb2020633bb61
Jared Bean <jibean19@gmail.com>
2025-02-25 15:41:52 -0500

    Make initial dotfiles lighter to initialize (#4)
    Also give an easy utility to install heavier, but still useful, utilities.

--- deps.sh
+++ deps.sh
@@ -1,10 +1,28 @@
 #! /usr/bin/env sh
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <REPO_ROOT>" >&2
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <REPO_ROOT> [CONFIG_FILE]" >&2
   exit 1
 fi
 readonly REPO_ROOT="$1"
-. "${REPO_ROOT}/deps/lib.sh"
+readonly DEPS_ROOT="${REPO_ROOT}/deps"
+# shellcheck disable=SC1091
+. "${DEPS_ROOT}/lib.sh"
+# We want word splitting in the 'ls ...' so basename lops off the '.txt' of
+# each word/filename
+# shellcheck disable=SC2046
+CONFIGS=$(basename -s .txt $(ls "${DEPS_ROOT}/"*.txt) | tr '\n' ' ')
+readonly CONFIGS
+if [ $# -eq 2 ]; then
+  if ! contains "$2" "${CONFIGS}"; then
+    echo "Usage: $0 <REPO_ROOT> [CONFIG_FILE]" >&2
+    echo "Available configs: "
+    echo "${CONFIGS}"
+    exit 1
+  fi
+  readonly CONFIG_FILE="${DEPS_ROOT}/$2.txt"
+else
+  readonly CONFIG_FILE="${DEPS_ROOT}/config.txt"
+fi
 
 echo "Reading configuration"
 while read -r provider commands; do
@@ -12,21 +30,21 @@ while read -r provider commands; do
     apt)
       echo "Installing apt commands '${commands}'"
       # shellcheck disable=SC2086
-      "${REPO_ROOT}/deps/providers/apt.sh" "${REPO_ROOT}" ${commands}
+      "${DEPS_ROOT}/providers/apt.sh" "${REPO_ROOT}" ${commands}
       ;;
     cargo)
       echo "Installing cargo commands '${commands}'"
       # shellcheck disable=SC2086
-      "${REPO_ROOT}/deps/providers/cargo.sh" "${REPO_ROOT}" ${commands}
+      "${DEPS_ROOT}/providers/cargo.sh" "${REPO_ROOT}" ${commands}
       ;;
     other)
       echo "Installing other commands '${commands}'"
       # shellcheck disable=SC2086
-      "${REPO_ROOT}/deps/providers/other.sh" "${REPO_ROOT}" ${commands}
+      "${DEPS_ROOT}/providers/other.sh" "${REPO_ROOT}" ${commands}
       ;;
   esac
 done << EOF
-$(readDepsConfig "${REPO_ROOT}/deps/config.txt")
+$(readDepsConfig "${CONFIG_FILE}")
 EOF
 
 if isInstalled kitty; then
